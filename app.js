cceb342950863ce6c0b01bbc4f2c6f57fc86eacb
Anup Singh <anups5246@gmail.com>
2021-08-16 00:16:56 +0530

    test

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 let validation = document.querySelectorAll('.validation');
 let radio = document.querySelectorAll('.radio');
 let custom = document.querySelector('#custom');
-
+let billamount = document.querySelector('#bill');
+let resetBtn = document.querySelector('.reset-btn');
 let num = 0;
+let finalAmount = 0;
+
+let printTip = document.querySelector('.tip-value');
+let printAmount = document.querySelector('.total-value');
+let caution = document.querySelector('.alert');
 
 let tipPercents = [5, 10, 15, 25, 50];
-let tipGiven = 0;
+
 let peopleCount = document.querySelector('#people');
-let tip = document.querySelector('.tip-value');
+
+peopleCount.addEventListener('change', () => {
+    let count = document.querySelector('#people').value;
+    if (count == 0) {
+        caution.classList.remove('none');
+    } else {
+        caution.classList.add('none');
+    }
+})
 
 for (let i = 0; i < radio.length; i++) {
     radio[i].addEventListener('click', () => {
-        num = tipPercents[i];
+        return num = tipPercents[i];
     });
 }
 
 //when one percent is already selected and custom is focused then remove the selected percent
 
-custom.addEventListener('focus', () => {
+custom.addEventListener('input', () => {
     for (let i = 0; i < radio.length; i++) {
         if (radio[i].checked) {
             radio[i].checked = false;
-
+            num = Number(custom.value);
+            calculate();
+        } else {
+            num = Number(custom.value);
+            calculate();
         }
     }
 });
-
-
 
 //validation of numerical keys only in input
 function validator(e) {
@@ -40,19 +56,39 @@ function validator(e) {
 for (let i = 0; i < validation.length; i++) {
     validation[i].addEventListener('keypress', validator);
 }
-let percent = tipPercents[num];
-let printTip = document.getElementsByClassName('tip-value');
 
 function calculate() {
     let billAmount = Number(document.getElementById('bill').value);
-    let percents = Number(percent);
+    let percents = Number(num);
     let peopleCout = Number(document.getElementById('people').value);
 
-    let tip = (billAmount * (percent / 100)) / peopleCout;
+    let tip = (Math.round((billAmount * (percents / 100)) / peopleCout * 100) / 100);
+    let totalAmount = (billAmount / peopleCout) + tip;
+    finalAmount = Math.round(totalAmount * 100) / 100;
 
-    printTip.innerHTML = tip;
-    console.log(tip);
-    console.log(billAmount, percents, peopleCount);
-
+    printTip.innerHTML = "$" + " " + tip;
+    printAmount.innerHTML = "$" + " " + finalAmount;
 }
-peopleCount.oninput = calculate;
+
+
+peopleCount.onchange = calculate;
+billamount.onchange = calculate;
+for (let i = 0; i < radio.length; i++) {
+    radio[i].onchange = calculate;
+}
+
+resetBtn.addEventListener('click', () => {
+    billamount.value = 0;
+    peopleCount.value = 1;
+
+    printTip.innerHTML = `$ 0.00`;
+    printAmount.innerHTML = `$ 0.00`;
+    custom.value = null;
+    for (let i = 0; i < radio.length; i++) {
+        if (radio[i].checked) {
+            radio[i].checked = false;
+
+        }
+    }
+
+})
